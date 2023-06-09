bb760dbde8916b8ccc9ff5dbddf6e9ff421e11dd
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-06-09 23:18:49 +0530

    [Amit Kumar]Added : Take two line and check their length are equal or not

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -1,7 +1,14 @@
-﻿namespace LineComparison
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LineComparison
 {
     internal class Program
     {
+        public static double GetLineLength(double x1, double y1, double x2, double y2)
+        {
+            double length = Math.Sqrt((Math.Pow((x2 - x1), 2)) + (Math.Pow((y2 - y1), 2)));
+            return length;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation");
@@ -18,9 +25,32 @@
             Console.WriteLine("Enter the value of Y2");
             double y2 = Convert.ToDouble(Console.ReadLine());
 
+            Console.WriteLine("================2nd Line Point===============");
+            Console.WriteLine("Enter the value of X3");
+            double x3 = Convert.ToDouble(Console.ReadLine());
 
-            double lineLength = Math.Sqrt((Math.Pow((x2 - x1), 2)) + (Math.Pow((y2 - y1), 2)));
-            Console.WriteLine($"The Length of the Line is {lineLength}");
+            Console.WriteLine("Enter the value of Y3");
+            double y3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of X4");
+            double x4 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of Y4");
+            double y4 = Convert.ToDouble(Console.ReadLine());
+
+            
+            double firstLineLength = GetLineLength(x1, y1, x2, y2);
+            double secondLineLength = GetLineLength(x3, y3, x4, y4);
+
+            if (firstLineLength == secondLineLength)
+            {
+                Console.WriteLine("The line length are equal");
+            }
+            else
+            {
+                Console.WriteLine("The line length are NOT equal");
+            }
         }
+       
     }
 }
