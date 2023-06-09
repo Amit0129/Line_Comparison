3888fb611a2d42bbc91f71c2ee496fe46d5ddfe0
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-06-09 23:03:37 +0530

    [Amit Kumar]Added : Calculate line length using Cartesian syatem

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation");
+
+            Console.WriteLine("Enter the value of X1");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of Y1");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of X2");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of Y2");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+
+            double lineLength = Math.Sqrt((Math.Pow((x2 - x1), 2)) + (Math.Pow((y2 - y1), 2)));
+            Console.WriteLine($"The Length of the Line is {lineLength}");
         }
     }
 }
